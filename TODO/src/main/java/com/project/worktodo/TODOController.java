be2e7b55a46cc8e3aa836a7411f773e3a67e84df
przemek905 <przemek920@wp.pl>
2017-08-16 16:45:27 +0200

    Add CrossOrigin annotation

--- TODO/src/main/java/com/project/worktodo/TODOController.java
+++ TODO/src/main/java/com/project/worktodo/TODOController.java
@@ -1,14 +1,16 @@
 package com.project.worktodo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TODOController {
 
-	@RequestMapping("/hello")
-	public String hello(@RequestParam String name) {
+	@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping("/hello")
+	public String hello(@RequestParam(required=false, defaultValue="World") String name) {
 		return "Hello "+name;
 	}
 }
--- TODO/src/main/java/com/project/worktodo/TODORepository.java
+++ TODO/src/main/java/com/project/worktodo/TODORepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "todos", path = "todos")
 public interface TODORepository extends PagingAndSortingRepository<TODO, Long> {
 	
